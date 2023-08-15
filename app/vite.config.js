318b2991dd3114821902386b369c8e7fd5756c84
Ideacloud-ng <sqaleup@gmail.com>
2023-08-15 08:49:32 +0000

    (update): PWA

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,48 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const manifestForPlugin = {
+	registerType: "prompt",
+	includeAssets: ["favicon.png", "apple-touch-icon.png", "masked_icon.png"],
+	manifest: {
+		"name": "Identify.ai",
+	"short_name": "Identify.ai",
+	"description": "The only criminal identification system you'll need",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
